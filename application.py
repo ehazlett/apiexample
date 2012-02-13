2b29ce3aaf9140cef0f4ccb6df751ce18153e120
Evan Hazlett <ejhazlett@gmail.com>
2012-02-13 17:48:49 -0500

    more work on api -- will now query mongodb with limits and offsets

--- application.py
+++ application.py
@@ -4,29 +4,29 @@ from flask import g
 from flask.views import MethodView
 from decorators import throttle
 from flask.ext.pymongo import PyMongo
-from pymongo import json_util
 from flask import json
+from utils import redis_connect, bson_to_json
 import redis
 import time
+from blueprints.trials import trials_blueprint
+from apiexample import create_app
 
-app = Flask(__name__)
-app.debug = True
+app = create_app()
+# blueprints
+app.register_blueprint(trials_blueprint, url_prefix='/trials')
 mongo = PyMongo(app)
-rds = redis.Redis(db=12)
+rds = redis_connect()
 
 @app.before_request
 def before():
     addr = request.remote_addr
     count = rds.incr('visitors:{0}'.format(addr))
     if count == 1:
-        rds.expire('visitors:{0}'.format(addr), 20)
+        rds.expire('visitors:{0}'.format(addr), 60)
     rds.incr('visits')
 
-def bson_to_json(data):
-    return json.dumps(data, default=json_util.default, sort_keys=True)
-
 class SampleAPI(MethodView):
-    @throttle(rds=rds)
+    #@throttle(rds=rds)
     def get(self, id=None):
         if id:
             data = mongo.db.records.find_one({'r_id': id})
@@ -44,7 +44,7 @@ class SampleAPI(MethodView):
         return jsonify({'r_id': r_id})
     
 class MetricAPI(MethodView):
-    @throttle(rds=rds, limit=10)
+    #@throttle(rds=rds, limit=10)
     def get(self):
         return jsonify({'visitors': rds.get('visits')})
 
@@ -54,3 +54,5 @@ app.add_url_rule('/visitors', view_func=MetricAPI.as_view('metrics'), methods=['
 
 if __name__=='__main__':
     app.run(host='0.0.0.0')
+
+
--- decorators.py
+++ decorators.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python
 from functools import wraps
 from flask import request, Response, json
+from flask import g
+from utils import redis_connect
 
-def throttle(rds=None, limit=50):
+rds = redis_connect()
+
+def throttle(limit=50):
     def inner_throttle(f):
         def decorated(*args, **kwargs):
             if int(rds.get('visitors:{0}'.format(str(request.remote_addr)))) > limit:
